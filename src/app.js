5c8dbe779b5e88479e95424ebf8f4df9b0f9dd36
Sonal <sonal.cmendis@gmail.com>
2019-10-28 15:18:23 +0530

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ import configureStore from './store/configureStore';
 import { addExpense, removeExpense, editExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
+import validator from 'validator';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import validator from 'validator';
+
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
 /***
  * NOTE
  * We don't need to import the reducers here because the reducers have already been imported in store/configureStore.js
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,52 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	mode: 'development',
-	module: {
-		rules: [
-			{
-				loader: 'babel-loader',
-				test: /\.js$/,
-				exclude: /node_modules/
-			},
-			{
-				test: /\.s?css$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
-		]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
-	}
+module.exports = env => {
+	const isProduction = env === 'production';
+
+	console.log('env', env);
+	return {
+		entry: './src/app.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		mode: 'development',
+		module: {
+			rules: [
+				{
+					loader: 'babel-loader',
+					test: /\.js$/,
+					exclude: /node_modules/
+				},
+				{
+					test: /\.s?css$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+							options: {
+								publicPath: (resourcePath, context) => {
+									return path.relative(path.dirname(resourcePath), context) + '/';
+								}
+							}
+						},
+						'css-loader',
+						'sass-loader'
+					]
+				}
+			]
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				// Options similar to the same options in webpackOptions.output
+				// all options are optional
+				filename: 'styles.css'
+			})
+		],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true
+		}
+	};
 };
